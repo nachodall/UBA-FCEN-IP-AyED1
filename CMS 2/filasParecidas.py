4e71ad865e3ca4de2f9a38f65c035af09bba6813
nacho <91359628+nachodall@users.noreply.github.com>
2023-05-29 11:23:36 -0300

    Add files via upload

---
+++ CMS 2/filasParecidas.py
@@ -0,0 +1,31 @@
+from typing import List
+
+# Aclaración: Debido a la versión de Python del CMS, para el tipo Lista, la sintaxis de la definición de tipos que deben usar es la siguiente:
+# l: List[int]  <--Este es un ejemplo para una lista de enteros.
+# Respetar esta sintaxis, ya que el CMS dirá que no pasó ningún test si usan otra notación.
+def filasParecidas(matriz: List[List[int]]) -> bool :
+
+  n: int = matriz[1][0] - matriz[0][0]
+  
+  for i in range (1,len(matriz),1):
+    for j in range (0,len(matriz[i]),1):
+      if matriz[i][j] != (matriz[i-1][j] + n):
+        return False
+      
+  return True
+
+
+
+if __name__ == '__main__':
+  filas = int(input())
+  columnas = int(input())
+ 
+  matriz = []
+ 
+  for i in range(filas):         
+    fila = input()
+    if len(fila.split()) != columnas:
+      print("Fila " + str(i) + " no contiene la cantidad adecuada de columnas")
+    matriz.append([int(j) for j in fila.split()])
+  
+  print(filasParecidas(matriz))
