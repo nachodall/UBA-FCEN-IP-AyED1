c2659e97af75a38dc1873877ca212b642e4c425b
nacho <91359628+nachodall@users.noreply.github.com>
2023-06-07 18:16:55 -0300

    Update p10e19.py

--- Guía 10: Archivos, Pilas, Colas y Diccionarios/p10e19.py
+++ Guía 10: Archivos, Pilas, Colas y Diccionarios/p10e19.py
@@ -18,15 +18,21 @@ def promedioEstudiante() -> float:
         if campos[0] in diccionario:
             diccionario[campos[0]] += float(campos[3]) #voy acumulando las notas
         else:
-            diccionario[campos[0]] = float(campos[3])
+            diccionario[campos[0]] = float(campos[3]) #siempre q llamamos al campo donde esten las notas le hardcodeamos el tipo float ya que por defecto en los csv viene como str
     
-    for alumno in diccionario.keys():
+    for alumno in diccionario.keys(): #recorro cada alumno y calculo su promedio
         if apariciones(notas,alumno) != 0:
-            diccionario[alumno] = float(diccionario[alumno]) / apariciones(notas,alumno)
+            diccionario[alumno] = float(diccionario[alumno]) / apariciones(notas,alumno) #el divisor del promedio es la cant. de apariciones en las notas
         else:
             diccionario[alumno] = -1
     
     return diccionario
+
+
+        
+    
+    
+print(promedioEstudiante())
 
 
         
