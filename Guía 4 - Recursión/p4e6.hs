b8aa3663ae5cc063a07960b5cc5213fc7b458ce8
nacho <91359628+nachodall@users.noreply.github.com>
2023-06-10 17:31:38 -0300

    Update p4e6.hs

--- Guía 4 - Recursión/p4e6.hs
+++ Guía 4 - Recursión/p4e6.hs
@@ -2,15 +2,9 @@
 de un numero natural. Para esta funcion pueden utilizar div y mod.--}
 
 sumaDigitos :: Integer -> Integer 
-sumaDigitos n  | n < 10 = 1 
-               | otherwise = 1 + sumaDigitos (sacoUltimoDigito n) --esto es como un contador en imperativo. el primer numero indica de a cuanto quiero sumar
+sumaDigitos n  | n == 0 = 0 
+               | otherwise = mod n 10 + sumaDigitos (sacoUltimoDigito n) --esto es como un contador en imperativo. el primer numero indica de a cuanto quiero sumar
 
 sacoUltimoDigito :: Integer -> Integer
 sacoUltimoDigito x = div x 10
 
-
-{--5678 res 0
-567 res 1
-56 res 2
-5 res 3
-- res 4--} 
