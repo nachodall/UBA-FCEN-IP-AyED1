2dc90cb4232d6f155cf0395a9a6cde0b1a8fc6c3
nacho <91359628+nachodall@users.noreply.github.com>
2023-06-10 22:30:21 -0300

    Update p4e10.hs

--- Guía 4 - Recursión/p4e10.hs
+++ Guía 4 - Recursión/p4e10.hs
@@ -1,27 +1,25 @@
 {--Implementar y dar el tipo de las siguientes funciones--}
 
 --a)
-f1 :: Integer -> Integer
-f1 n | n == 0 = 1
-     | otherwise = 2 ^ n + f1 (n-1)
+f1 :: Int -> Int
+f1 n = f1Aux n 0
 
+f1Aux :: Int -> Int -> Int
+f1Aux n i | n == i = 2^i 
+          | otherwise = 2^i + f1Aux n (i+1)
 --b)
-f2 :: Integer -> Integer -> Integer
-f2 n q | n == 1 = q 
-       | otherwise = q ^ n + f2 (n-1) q
+f2 :: Int -> Float -> Float
+f2 n q | n == 1 = q^n 
+       | otherwise = q^n + f2 (n-1) q
 
 --c)
-f3 :: Integer -> Integer -> Integer
-f3 n q = f3aux (2*n) q
-
-f3aux :: Integer -> Integer -> Integer
-f3aux n q | n == 1 = q 
-          | otherwise = q ^ n + f3aux (n-1) q
+f3 :: Int -> Float -> Float
+f3 n q = f2 (2*n) q
 
 --d)
-f4 :: Integer -> Integer -> Integer
-f4 n q = f4aux (2*n) n q
+f4 :: Int -> Float -> Float
+f4 n q = f4Aux n (2*n) q 
 
-f4aux :: Integer -> Integer -> Integer -> Integer
-f4aux sup inf q | sup == inf = q^sup 
-                | otherwise = q^sup + f4aux (sup -1) inf q
+f4Aux :: Int -> Int -> Float -> Float
+f4Aux i n q | i == n = q^i 
+            | otherwise = q^i + f4Aux (i+1) n q 
