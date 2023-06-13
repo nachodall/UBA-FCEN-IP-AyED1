dbe4e286149f7d777adaa5577648879792c6cca3
nacho <91359628+nachodall@users.noreply.github.com>
2023-06-13 02:33:01 -0300

    Update p5e3.hs

--- Guía 5 - Recursión sobre listas/p5e3.hs
+++ Guía 5 - Recursión sobre listas/p5e3.hs
@@ -9,45 +9,6 @@ ultimo [x] = x
 ultimo (x:xs) = ultimo xs
 
 -- 3.1 sumatoria :: [Integer] -> Integer segun la siguiente especificacion:
-sumatoria :: [Int] -> Int
-sumatoria [] = 0
-sumatoria (x:xs) = x + sumatoria xs
-
---3.2 productoria :: [Integer] -> Integer segun la siguiente especificacion:
-productoria :: [Int] -> Int
-productoria [] = 0
-productoria [x] = x
-productoria (x:xs) = x * productoria xs
-
---3.3 maximo :: [Integer] -> Integer segun la siguiente especificacion:
-maximo :: [Integer] -> Integer
-maximo [x] = x
-maximo (x:y:xs) | x > y = maximo (x:xs)  
-                | otherwise = maximo (y:xs)
-
---3.4 sumarN :: Integer -> [Integer] -> [Integer] segun la siguiente especificacion:
-sumarN :: Integer -> [Integer] -> [Integer]
-sumarN _ [] = []
-sumarN n (x:xs) = (x + n) : sumarN n xs
-
---3.5 sumarElPrimero :: [Integer] -> [Integer] segun la siguiente especificacion:
-sumarElPrimero :: [Integer] -> [Integer]
-sumarElPrimero (x:xs) = sumarN x (x:xs)
-
---3.6 sumarElUltimo :: [Integer] -> [Integer] segun la siguiente especificacion:
-sumarElUltimo :: [Integer] -> [Integer]
-sumarElUltimo (x:xs) = sumarN (ultimo xs) (x:xs)
-
---auxiliares
-longitud :: [t] -> Integer
-longitud []  = 0
-longitud (x:xs) = 1 + longitud xs  
-
-ultimo :: [t] -> t 
-ultimo [x] = x
-ultimo (x:xs) = ultimo xs
-
--- 3.1 sumatoria :: [Integer] -> Integer segun la siguiente especificacion:
 sumatoria :: [Integer] -> Integer
 sumatoria (x:xs) | longitud xs == 0 = x 
                  | otherwise = x + sumatoria xs
