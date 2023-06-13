21c3ebebe1cf3521286e4ca79e0580b0d3918225
nacho <91359628+nachodall@users.noreply.github.com>
2023-06-13 01:25:04 -0300

    Update p5e1.hs

--- Guía 5 - Recursión sobre listas/p5e1.hs
+++ Guía 5 - Recursión sobre listas/p5e1.hs
@@ -9,23 +9,13 @@ ultimo (x:xs) | longitud xs == 0 = x
               | otherwise = ultimo xs
 
 --1.3
-s1 = []
-principio :: [t] -> [t]
-principio (x:xs) = principioAux (x:xs) s1 
-
-principioAux :: [t] -> [t] -> [t]
-principioAux (x:xs) s1 | longitud xs == 0 = reverso s1
-                       | otherwise = principioAux xs (x:s1)
+principio::[t]->[t]
+principio [x] = []
+principio (x:xs) = x : principio xs
 
 --1.4
-sv = []
-reverso :: [t] -> [t]
-reverso (x:xs) = reversoAux sv (x:xs)
+reverso::[t]->[t]
+reverso [] = []
+reverso (x:xs) = reverso xs ++ [x]
+--------------------------------------
 
-reversoAux :: [t] -> [t] -> [t]
-reversoAux sv sl | longitud sl == 0 = sv  
-                 | otherwise = reversoAux (head sl : sv) (tail sl) 
-
-reversoPM :: [t] -> [t]
-reversoPM [] = []
-reversoPM (x:xs) = (reversoPM xs)  ++ [x]
